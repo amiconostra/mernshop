4fe063386484190a29a67315d37dfc1f700d4d79
amiconostra <nostramail@protonmail.com>
2021-01-09 17:37:21 -0500

    Added CSRF Protection

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const flash = require('connect-flash');
+const csrf = require('csurf');
 
 // Models
 const User = require('./models/user');
@@ -14,13 +15,17 @@ const User = require('./models/user');
 // Database
 const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`;
 
-const app = express();
-
 // Session DB
 const store = new MongoDBStore({
     uri: MONGODB_URI,
     collection: 'sessions'
 });
+
+// CSRF Settings
+const csrfProtection = csrf();
+
+// Express APP
+const app = express();
 
 // Express Settings
 app.set('view engine', 'ejs');
@@ -39,6 +44,7 @@ app.use(session({
     store: store,
     // cookie: { secure: true }
 }));
+app.use(csrfProtection);
 app.use(flash());
 
 // User Handler
@@ -55,6 +61,13 @@ app.use((req, res, next) => {
         .catch(err => {
             console.log(err);
         });
+});
+
+// Shared Across Views (Requests)
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isAuthenticated;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 // Routes
