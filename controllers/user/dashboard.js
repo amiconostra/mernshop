64b77aadbfc85a01e5827780a6623431c2d29e49
amiconostra <nostramail@protonmail.com>
2021-01-16 20:16:25 -0500

    Improved Verification System

--- controllers/user/dashboard.js
+++ controllers/user/dashboard.js
@@ -2,7 +2,16 @@ const path = require('path');
 const rootdir = require('../../helpers/rootdir');
 const config = require(path.join(rootdir, 'config.json'));
 
+// Models
+const User = require(rootdir, 'models', 'user');
+
 exports.getDashboard = (req, res, next) => {
+    if(!req.user.verified) {
+        req.session.isAuthenticated = false;
+        req.flash('error', 'Please Verify your account');
+        res.redirect('/login');
+    }
+
     res.render(path.join(config.theme.name, 'dashboard'), {
         pageTitle: 'Dashboard',
         path: '/dashboard'
